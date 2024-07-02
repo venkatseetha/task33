4df9cf36b9117186d102301efc626892d5a08de6
sai <venkatnaidu15@yahoo.com>
2024-07-02 16:14:20 +0530

    allfiles

---
+++ company-java-project/src/main/java/com/company/Calculator.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Calculator {
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero");
+        }
+        return a / b;
+    }
+}
+
