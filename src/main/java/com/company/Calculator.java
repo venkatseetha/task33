33637ee4ad6509cc49dea1b777bc66571d0e0e5c
ReddyNaveen9966 <104193347+ReddyNaveen9966@users.noreply.github.com>
2024-07-03 20:29:51 +0530

    Create Calculator.java

---
+++ src/main/java/com/company/Calculator.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Calculator {
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero");
+        }
+        return a / b;
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
